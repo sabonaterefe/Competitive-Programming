efff28ecb54f61cc93b107e15e00690f4754556e
sabona <98513125+sabonaterefe@users.noreply.github.com>
2023-12-01 21:26:07 +0300

    Added solution for Verifying an Alien Dictionary

--- Leetcode -Solutions/verifying-an-alien-dictionary.py
+++ Leetcode -Solutions/verifying-an-alien-dictionary.py
@@ -1,7 +1,17 @@
 class Solution:
-        def isAlienSorted(self, words: List[str], order: str) -> bool:
-                 table = str.maketrans(dict(zip(order, sorted(order))))
-                 return all(a.translate(table) <= b.translate(table) for a, b in zip(words, words[1:]))
-       
-  
+    def isAlienSorted(self, words, order):
+        alien_dict = {char: i for i, char in enumerate(order)}
+
+        for i in range(1, len(words)):
+            prev_word = words[i - 1]
+            curr_word = words[i]
+            
         
+            for j in range(len(prev_word)):
+           
+                if j >= len(curr_word) or alien_dict[prev_word[j]] > alien_dict[curr_word[j]]:
+                    return False
+                elif alien_dict[prev_word[j]] < alien_dict[curr_word[j]]:
+                    break
+        
+        return True

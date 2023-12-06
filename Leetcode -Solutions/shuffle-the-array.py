ad45543eaaebf182fe5b6fb10efc929f8821d21e
sabona <98513125+sabonaterefe@users.noreply.github.com>
2023-12-06 17:15:02 +0300

    Added solution for Shuffle the Array

---
+++ Leetcode -Solutions/shuffle-the-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        n = len(nums)//2
+        result = []
+        for i in range(n):
+            result.append(nums[i])
+            result.append(nums[i+n])
+        return result
+
+        

92e6ff91fe3c65cf098bfc07861888f241c9bd56
sabona <98513125+sabonaterefe@users.noreply.github.com>
2023-12-12 17:16:46 +0300

    Added solution for Happy Number

---
+++ Leetcode -Solutions/happy-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n!=1:
+            n = sum(int(digit)**2 for digit in str(n))
+            if n in seen:
+                return False
+            else:
+                seen.add(n)
+        return True    
+        

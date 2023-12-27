985ad56488c73a41572c25e9f3935217050303bf
sabona <98513125+sabonaterefe@users.noreply.github.com>
2023-12-27 17:52:32 +0300

    Added solution for Sort the People

--- Leetcode -Solutions/sort-the-people.py
+++ Leetcode -Solutions/sort-the-people.py
@@ -1,16 +1,18 @@
 class Solution:
     def sortPeople(self, names, heights):
         n = len(heights)
-        
-        for i in range(n - 1):
-            swap  =False
-            for j in range(n - i - 1):
-                if heights[j] < heights[j + 1]:
-                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
-                   
-                    names[j], names[j + 1] = names[j + 1], names[j]
-                    swap = True
-            if not swap:
-                break
-        
+
+        for i in range(1, n):
+            key_height = heights[i]
+            key_name = names[i]
+            j = i - 1
+
+            while j >= 0 and heights[j] < key_height:
+                heights[j + 1] = heights[j]
+                names[j + 1] = names[j]
+                j -= 1
+
+            heights[j + 1] = key_height
+            names[j + 1] = key_name
+
         return names

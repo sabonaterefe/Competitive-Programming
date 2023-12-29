c09d150b396b36a9b7c7e8e09bd97d1b739b465e
sabona <98513125+sabonaterefe@users.noreply.github.com>
2023-12-29 17:14:25 +0300

    Added solution for Largest Number

---
+++ Leetcode -Solutions/largest-number.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class LargerNumKey(str):
+    def __lt__(x, y):
+        return x + y > y + x
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = [str(num) for num in nums]
+        nums.sort(key=LargerNumKey)
+        largest_num = ''.join(nums)
+    
+        return "0" if largest_num[0] == "0" else largest_num

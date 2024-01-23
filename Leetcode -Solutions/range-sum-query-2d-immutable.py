f0b2ab6c4ea11bd731d0f30fe8f91e917443e14a
sabona <98513125+sabonaterefe@users.noreply.github.com>
2024-01-23 19:39:47 +0300

    Added solution for Range Sum Query 2D - Immutable

---
+++ Leetcode -Solutions/range-sum-query-2d-immutable.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class NumMatrix:
+    def __init__(self, matrix: List[List[int]]):
+        if not matrix or not matrix[0]:
+            self.cumulative_sum = None
+        else:
+            rows, cols = len(matrix), len(matrix[0])
+            self.cumulative_sum = [[0] * (cols + 1) for _ in range(rows + 1)]
+            for i in range(1, rows + 1):
+                for j in range(1, cols + 1):
+                    self.cumulative_sum[i][j] = (
+                        matrix[i - 1][j - 1]
+                        + self.cumulative_sum[i - 1][j]
+                        + self.cumulative_sum[i][j - 1]
+                        - self.cumulative_sum[i - 1][j - 1]
+                    )
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        if not self.cumulative_sum:
+            return 0
+        return (
+            self.cumulative_sum[row2 + 1][col2 + 1]
+            - self.cumulative_sum[row1][col2 + 1]
+            - self.cumulative_sum[row2 + 1][col1]
+            + self.cumulative_sum[row1][col1]
+        )

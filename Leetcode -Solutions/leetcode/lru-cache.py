121a2252714a6e24ee3c67a149f52b3a4648a9af
sabona <98513125+sabonaterefe@users.noreply.github.com>
2024-02-13 15:05:30 +0300

    Add solution for LRU Cache

---
+++ Leetcode -Solutions/leetcode/lru-cache.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, key: int, val: int, prev=None, next=None):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self._remove(node)
+        self._add(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self.cache[key] = node
+        self._add(node)
+        if len(self.cache) > self.cap:
+            node_to_remove = self.head.next
+            self._remove(node_to_remove)
+            del self.cache[node_to_remove.key]
+
+    def _add(self, node: Node) -> None:
+        last_node = self.tail.prev
+        last_node.next = node
+        node.prev = last_node
+        node.next = self.tail
+        self.tail.prev = node
+
+    def _remove(self, node: Node) -> None:
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node

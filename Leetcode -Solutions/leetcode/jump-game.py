09e0e3548ba39163e9fa3efffd713d8c7259ad5e
sabona <98513125+sabonaterefe@users.noreply.github.com>
2024-02-23 14:40:24 +0300

    Add solution for Jump Game

---
+++ Leetcode -Solutions/leetcode/jump-game.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reachable = 0
+        for i in range(len(nums)):
+            if i > max_reachable:
+                return False
+            max_reachable = max(max_reachable, i + nums[i])
+        return True

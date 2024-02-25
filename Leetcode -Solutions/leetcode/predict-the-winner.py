25df0d0d6437f3cb2e02d90eeadf2a8174076518
sabona <98513125+sabonaterefe@users.noreply.github.com>
2024-02-25 19:51:35 +0300

    Add solution for Predict the Winner

---
+++ Leetcode -Solutions/leetcode/predict-the-winner.py
@@ -0,0 +1,11 @@
+class Solution:
+    def predictTheWinner(self, nums) -> bool:
+        n = len(nums)
+        dp = [0] * n
+
+        for i in range(n-1, -1, -1):
+            dp[i] = nums[i]
+            for j in range(i+1, n):
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j-1])
+
+        return dp[n-1] >= 0
